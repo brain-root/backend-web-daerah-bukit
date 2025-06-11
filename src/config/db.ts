ac41cfe97765c66d27be6a959c961ec6010bbdcd
merchan <gifranokta@student.unp.ac.id>
2025-06-11 22:58:35 +0700

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,11 +6,11 @@ dotenv.config();
 
 // Database configuration
 const dbConfig = {
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "aws-mysql.cnoiawuws42w.ap-southeast-1.rds.amazonaws.com",
   port: parseInt(process.env.DB_PORT || "3306", 10), 
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "webdaerah",
+  user: process.env.DB_USER || "adminrian001",
+  password: process.env.DB_PASSWORD || "MySQLamazonadminrian001",
+  database: process.env.DB_NAME || "solok_selatan_db",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
